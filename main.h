41e7334d6021c74698848600db808de1e17be0c9
kiriroN <marykiriro1@gmail.com>
2023-10-18 00:57:52 -0700

    main.ch

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+int printf_char(va_list vau);
+int _strlenc(const char *str);
+int _strlen(char *str);
+int printf_string(va_list vau);
+int print_20(void);
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
